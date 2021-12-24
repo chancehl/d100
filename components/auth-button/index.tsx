2a4ab4849181193ad1096f97ec43d9030988eb9e
Chance Linz <clinz.dev@gmail.com>
2021-12-24 13:07:07 -0800

    simplifying auth button

--- components/auth-button/index.tsx
+++ components/auth-button/index.tsx
@@ -1,20 +1,13 @@
+import { useEffect } from 'react'
+
 import { useSession, signIn, signOut } from 'next-auth/react'
 
 export const AuthButton = () => {
     const { data } = useSession()
 
     if (data && data.user) {
-        return (
-            <>
-                Signed in as: {data.user.email} <button onClick={() => signOut()}>Sign out</button>
-            </>
-        )
+        return <button onClick={() => signOut()}>Sign out</button>
     }
 
-    return (
-        <>
-            Not signed in
-            <button onClick={() => signIn()}>Sign in</button>
-        </>
-    )
+    return <button onClick={() => signIn()}>Sign in</button>
 }
