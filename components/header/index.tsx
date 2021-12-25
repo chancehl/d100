5ebfe0c3bd35cebdf229e6b334205add4f9179bf
Chance Linz <clinz.dev@gmail.com>
2021-12-25 13:36:39 -0800

    consolodating components

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,20 +1,40 @@
-import { useSession } from 'next-auth/react'
-
-import { AuthButtons } from '../auth-buttons'
-import { ProfilePicture } from '../profile-picture'
-import { Nav } from '../nav'
-import { Logo } from '../logo'
+/* eslint-disable @next/next/no-img-element */
+import { signIn, signOut, useSession } from 'next-auth/react'
+import Link from 'next/link'
 
 export const Header = () => {
     const { data } = useSession()
 
     return (
         <header className="flex items-center justify-between pt-2 pb-2 pl-4 pr-4">
-            <Logo />
-            <Nav />
+            <Link href="/">
+                <a>D100</a>
+            </Link>
+            <nav>
+                <ul className="flex flex-row gap-[128px]">
+                    <li>
+                        <Link href="/popular">
+                            <a>Most popular</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/saved">
+                            <a>Saved</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/blog/hello-world">
+                            <a>Blog Post</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
             <div className="flex gap-2">
-                {data?.user && data.user.image && <ProfilePicture src={data.user.image} name={data.user.name} />}
-                <AuthButtons />
+                <div className="flex flex-row items-center">
+                    {data?.user?.name && <span className="mr-2 uppercase">{data.user.name}</span>}
+                    {data?.user?.image && <img className="w-10 h-10 rounded-full" alt="profile-picture" src={data.user.image} />}
+                </div>
+                {data?.user == null ? <button onClick={() => signIn()}>Sign in</button> : <button onClick={() => signOut()}>Sign out</button>}
             </div>
         </header>
     )
--- components/logo/index.tsx
+++
@@ -1,7 +0,0 @@
-import Link from 'next/link'
-
-export const Logo = () => (
-    <Link href="/">
-        <a>D100</a>
-    </Link>
-)
