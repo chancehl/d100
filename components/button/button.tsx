bbee343e31c58b9fa88d24d3b2f3654771c52970
Chance Linz <clinz.dev@gmail.com>
2022-01-30 13:55:55 -0800

    fix props in button component

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -20,14 +20,22 @@ export const Button = (props: ButtonProps) => {
     }
 }
 
-export const PrimaryButton = (props: ButtonProps) => (
-    <button {...props} className="text-center text-white font-semibold rounded-full pt-2 pb-2 pl-6 pr-6 bg-slate-900 hover:bg-slate-700">
-        {props.text}
-    </button>
-)
+export const PrimaryButton = (props: ButtonProps) => {
+    const { buttonType: _buttonType, text, ...rest } = props
 
-export const SecondaryButton = (props: ButtonProps) => (
-    <button {...props} className="text-center font-semibold rounded-full pt-2 pb-2 pl-6 pr-6 border-slate-900 border-solid border-2 hover:bg-slate-900 hover:text-white">
-        {props.text}
-    </button>
-)
+    return (
+        <button {...rest} className="text-center text-white font-semibold rounded-full pt-2 pb-2 pl-6 pr-6 bg-slate-900 hover:bg-slate-700">
+            {text}
+        </button>
+    )
+}
+
+export const SecondaryButton = (props: ButtonProps) => {
+    const { buttonType: _buttonType, text, ...rest } = props
+
+    return (
+        <button {...rest} className="text-center font-semibold rounded-full pt-2 pb-2 pl-6 pr-6 border-slate-900 border-solid border-2 hover:bg-slate-900 hover:text-white">
+            {text}
+        </button>
+    )
+}
