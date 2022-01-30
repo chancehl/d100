3947d8e85645622bc89b3b113f7ec05dad661fdb
Chance Linz <clinz.dev@gmail.com>
2022-01-30 09:52:55 -0800

    add button type prop to button component

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,5 +1,26 @@
 import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
 
-export const Button = (props: DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) => (
+type ButtonType = 'primary' | 'secondary' | 'tertiary'
+
+interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+    buttonType?: ButtonType
+}
+
+export const Button = (props: ButtonProps) => {
+    const type = props.buttonType ?? 'primary'
+
+    switch (type) {
+        case 'primary':
+            return <PrimaryButton {...props} />
+        case 'secondary':
+            return <PrimaryButton {...props} />
+        case 'tertiary':
+            return <PrimaryButton {...props} />
+        default:
+            return <PrimaryButton {...props} />
+    }
+}
+
+export const PrimaryButton = (props: ButtonProps) => (
     <button {...props} className="text-center text-white font-semibold rounded-full pt-3 pb-3 pl-6 pr-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" />
 )
