d763ebfeefd19a27c6b5c795ea16356f654c04c6
Chance Linz <clinz.dev@gmail.com>
2022-01-31 23:28:14 -0800

    add save functionality

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,11 +1,17 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Collection } from '@prisma/client'
+import { AiOutlineHeart, AiFillHeart, AiOutlineLoading3Quarters } from 'react-icons/ai'
 
-import client from '../prisma/client'
+import prisma from '../prisma/client'
 import { Button } from '../components/button/button'
+import { getSession, useSession } from 'next-auth/react'
+import { GetServerSideProps } from 'next/types'
 
-export const CollectionPage = ({ collection }: any) => {
+export const CollectionPage = ({ collection, user }: any) => {
+    const [saved, setSaved] = useState<boolean>(user?.saved?.find((savedCollection: any) => savedCollection.id === collection.id) ?? false)
+    const [saving, setSaving] = useState(false)
+
     useEffect(() => {
         try {
             // We don't need to await this. It can just happen in the background.
@@ -15,19 +21,43 @@ export const CollectionPage = ({ collection }: any) => {
         }
     }, [])
 
+    const toggleSave = async () => {
+        try {
+            setSaving(true)
+
+            await axios.post(`/api/user/saved?collectionId=${collection.id}`)
+
+            setSaved((current) => !current)
+        } catch (error: any) {
+            console.error('Encountered an error when unsaving this collection', error.message)
+
+            setSaved(user?.saved?.find((savedCollection: any) => savedCollection.id === collection.id))
+        } finally {
+            setSaving(false)
+        }
+    }
+
+    const renderSaveIcon = () =>
+        saving ? (
+            <AiOutlineLoading3Quarters className="animate-spin text-2xl" />
+        ) : saved ? (
+            <AiFillHeart className="text-4xl cursor-pointer" onClick={toggleSave} />
+        ) : (
+            <AiOutlineHeart className="cursor-pointer text-4xl" onClick={toggleSave} />
+        )
+
     return (
-        <div className="flex flex-col h-full">
-            <div>
-                <h1 className="text-6xl font-black uppercase">{collection.name}</h1>
-                <ul>
-                    {collection?.collectionItems?.length &&
-                        collection.collectionItems.map((item: any) => (
-                            <li key={item.id}>
-                                {item.value} {item.description ? `(${item.description})` : null}
-                            </li>
-                        ))}
-                </ul>
-            </div>
+        <div className="flex flex-col items-center h-full p-32">
+            <h1 className="text-6xl font-black uppercase">{collection.name}</h1>
+            <div>{renderSaveIcon()}</div>
+            <ul>
+                {collection?.collectionItems?.length &&
+                    collection.collectionItems.map((item: any) => (
+                        <li key={item.id}>
+                            {item.value} {item.description ? `(${item.description})` : null}
+                        </li>
+                    ))}
+            </ul>
             <div>
                 <Button text="Home" buttonType="secondary" />
             </div>
@@ -35,10 +65,27 @@ export const CollectionPage = ({ collection }: any) => {
     )
 }
 
-export const getStaticProps = async ({ params }: any) => {
-    const collection = await client.collection.findUnique({
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const session = await getSession({ req: context.req })
+
+    const collectionId = context.params?.id
+
+    let user = null
+
+    if (session && session.user?.email) {
+        user = await prisma.user.findUnique({
+            where: {
+                email: session.user.email,
+            },
+            include: {
+                saved: true,
+            },
+        })
+    }
+
+    const collection = await prisma.collection.findUnique({
         where: {
-            id: params.id,
+            id: collectionId as string,
         },
         select: {
             id: true,
@@ -55,19 +102,20 @@ export const getStaticProps = async ({ params }: any) => {
         },
     })
 
+    if (collection == null) {
+        return {
+            redirect: {
+                destination: '/home',
+                permanent: false,
+            },
+        }
+    }
+
     return {
         props: {
-            collection: collection ?? null,
+            collection,
+            user,
         },
-    }
-}
-
-export const getStaticPaths = async () => {
-    const collections = await client.collection.findMany()
-
-    return {
-        paths: collections.map((collection: Collection) => ({ params: { id: collection.id } })),
-        fallback: false,
     }
 }
 
