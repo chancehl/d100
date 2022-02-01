a98fde0ddeff02548757642c5c01f89f15a92b4d
Chance Linz <clinz.dev@gmail.com>
2022-01-31 22:31:30 -0800

    keep track of users last visit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,34 @@
 import NextAuth from 'next-auth'
 import DiscordProvider from 'next-auth/providers/discord'
 
+import prisma from '../../../prisma/client'
+
 export default NextAuth({
-    // Configure one or more authentication providers
     providers: [
         DiscordProvider({
             clientId: process.env.DISCORD_CLIENT_ID,
             clientSecret: process.env.DISCORD_CLIENT_SECRET,
         }),
-        // ...add more providers here
     ],
+    callbacks: {
+        async signIn({ user }) {
+            if (user.email) {
+                await prisma.user.upsert({
+                    where: {
+                        email: user.email,
+                    },
+                    create: {
+                        email: user.email,
+                        name: user.name,
+                        lastLogin: new Date().toISOString(),
+                    },
+                    update: {
+                        lastLogin: new Date().toISOString(),
+                    },
+                })
+            }
+
+            return true
+        },
+    },
 })
