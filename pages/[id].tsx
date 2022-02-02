d303bf5260533258c139d87d53ee48bc87822b64
Chance Linz <clinz.dev@gmail.com>
2022-02-01 20:53:56 -0800

    add upvote/downvote functionality

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,21 +1,30 @@
 import { useEffect, useState } from 'react'
+import { getSession } from 'next-auth/react'
+import { GetServerSideProps } from 'next/types'
+import Router from 'next/router'
 import axios from 'axios'
 import { AiOutlineHeart, AiFillHeart, AiOutlineLoading3Quarters } from 'react-icons/ai'
+import { BsHandIndexThumb, BsHandThumbsDownFill, BsHandThumbsUp, BsHandThumbsDown, BsHandThumbsUpFill } from 'react-icons/bs'
 import { Collection, CollectionItem } from '@prisma/client'
 
 import prisma from '../prisma/client'
 import { Button } from '../components/button/button'
-import { getSession } from 'next-auth/react'
-import { GetServerSideProps } from 'next/types'
 
 type CollectionPageProps = {
     collection: Collection & { items: CollectionItem[] }
     saved: boolean
+    liked: boolean
+    disliked: boolean
 }
 
-export const CollectionPage = ({ collection, saved: savedFromServer }: CollectionPageProps) => {
+export const CollectionPage = ({ collection, saved: savedFromServer, liked: likedFromServer, disliked: dislikedFromServer }: CollectionPageProps) => {
     const [saved, setSaved] = useState<boolean>(savedFromServer ?? false)
+    const [liked, setLiked] = useState<boolean>(likedFromServer ?? false)
+    const [disliked, setDisliked] = useState<boolean>(dislikedFromServer ?? false)
+
     const [saving, setSaving] = useState(false)
+    const [liking, setLiking] = useState(false)
+    const [disliking, setDisliking] = useState(false)
 
     useEffect(() => {
         try {
@@ -42,29 +51,86 @@ export const CollectionPage = ({ collection, saved: savedFromServer }: Collectio
         }
     }
 
+    const toggleLike = async () => {
+        try {
+            setLiking(true)
+
+            await axios.post(`/api/user/upvote?collectionId=${collection.id}`)
+
+            setLiked((current) => !current)
+        } catch (error: any) {
+            console.error('Encountered an error when unsaving this collection', error.message)
+
+            setLiked(likedFromServer)
+        } finally {
+            setLiking(false)
+        }
+    }
+
+    const toggleDislike = async () => {
+        try {
+            setDisliking(true)
+
+            await axios.post(`/api/user/downvote?collectionId=${collection.id}`)
+
+            setDisliked((current) => !current)
+        } catch (error: any) {
+            console.error('Encountered an error when unsaving this collection', error.message)
+
+            setDisliked(dislikedFromServer)
+        } finally {
+            setDisliking(false)
+        }
+    }
+
     const renderSaveIcon = () =>
         saving ? (
-            <AiOutlineLoading3Quarters className="animate-spin text-2xl" />
+            <AiOutlineLoading3Quarters className="animate-spin text-xl" />
         ) : saved ? (
             <AiFillHeart className="text-4xl cursor-pointer" onClick={toggleSave} />
         ) : (
             <AiOutlineHeart className="text-4xl cursor-pointer" onClick={toggleSave} />
         )
 
+    const renderThumbsUpIcon = () =>
+        liking ? (
+            <AiOutlineLoading3Quarters className="animate-spin text-xl" />
+        ) : liked ? (
+            <BsHandThumbsUpFill className="text-4xl cursor-pointer" onClick={toggleLike} />
+        ) : (
+            <BsHandThumbsUp className="text-4xl cursor-pointer" onClick={toggleLike} />
+        )
+
+    const renderThumbsDownIcon = () =>
+        disliking ? (
+            <AiOutlineLoading3Quarters className="animate-spin text-xl" />
+        ) : disliked ? (
+            <BsHandThumbsDownFill className="text-4xl cursor-pointer" onClick={toggleDislike} />
+        ) : (
+            <BsHandThumbsDown className="text-4xl cursor-pointer" onClick={toggleDislike} />
+        )
+
     return (
-        <div className="flex flex-col items-center h-full p-32">
-            <h1 className="text-6xl font-black uppercase">{collection.name}</h1>
-            <div className="flex flex-row justify-between">{renderSaveIcon()}</div>
-            <ul>
+        <div className="flex flex-col p-4 sm:p-16">
+            <div>
+                <h1 className="text-6xl font-black uppercase">{collection.name}</h1>
+                <div className="flex flex-row justify-between items-center border-t-2 border-slate-900 pt-8 mt-8">
+                    <span>{renderSaveIcon()}</span>
+                    <span>{renderThumbsUpIcon()}</span>
+                    <span>{renderThumbsDownIcon()}</span>
+                </div>
+            </div>
+            <ul className="pt-16">
                 {collection?.items?.length &&
                     collection.items.map((item) => (
                         <li key={item.id}>
-                            {item.value} {item.description ? `(${item.description})` : null}
+                            <span className="text-4xl font-bold">{item.value}</span>
+                            <p>{item.description ? `(${item.description})` : null}</p>
                         </li>
                     ))}
             </ul>
-            <div>
-                <Button text="Home" buttonType="secondary" />
+            <div className="mt-16">
+                <Button text="Home" buttonType="secondary" onClick={() => Router.push('/')} />
             </div>
         </div>
     )
@@ -84,6 +150,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
             },
             include: {
                 saved: true,
+                upvoted: true,
+                downvoted: true,
             },
         })
     }
@@ -120,6 +188,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         props: {
             collection,
             saved: user?.saved?.find((savedCollection) => savedCollection.id === collection.id) != null,
+            downvoted: user?.downvoted?.find((downvotedCollection) => downvotedCollection.id === collection.id) != null,
+            upvoted: user?.upvoted?.find((upvotedCollection) => upvotedCollection.id === collection.id) != null,
         },
     }
 }
---
+++ pages/api/user/downvote.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession, useSession } from 'next-auth/react'
+
+import prisma from '../../../prisma/client'
+import { parseQueryParam } from '../../../utils/parse-query-param'
+
+type ResponseData = { error: string } | { collection: any }
+
+export const downvote = async (req: NextApiRequest, res: NextApiResponse<ResponseData>) => {
+    if (req.method === 'POST') {
+        const session = await getSession({ req })
+
+        if (session && session?.user?.email) {
+            const id = parseQueryParam(req.query.collectionId)
+
+            if (id == null) {
+                res.status(403).json({ error: 'Could not locate collectionId' })
+
+                return
+            }
+
+            try {
+                const collection = await prisma.collection.findUnique({ where: { id } })
+
+                if (collection) {
+                    await prisma.user.update({
+                        where: {
+                            email: session.user.email,
+                        },
+                        data: {
+                            downvoted: {
+                                connect: {
+                                    id,
+                                },
+                            },
+                        },
+                    })
+                }
+
+                res.status(200).json({ collection })
+            } catch (error: any) {
+                res.status(500).json({ error: error.message })
+            }
+        } else {
+            res.status(401).json({ error: 'Unauthorized' })
+        }
+    } else {
+        res.status(400).json({ error: 'Invalid request. This endpoint only accepts POST requests.' })
+    }
+}
+
+export default downvote
